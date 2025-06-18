db1eb5acc6d1979b96fad0f24143b349a85c1dfc
Alex <xrlogic1@gmail.com>
2025-06-18 17:15:04 +0400

    Linq

--- Linq1/Linq1/Program.cs
+++ Linq1/Linq1/Program.cs
@@ -1,79 +1,108 @@
-﻿using Linq1;
-using Bogus;
-
-
-// string name = "alex";
+﻿// using Linq1;
+// using Bogus;
 //
-// var cap = name.Capitalize();
 //
-// Console.WriteLine(cap);
+// // string name = "alex";
+// //
+// // var cap = name.Capitalize();
+// //
+// // Console.WriteLine(cap);
+// //
+// // var list = new List<string>();
+// //
+// // list.Add("a");
+// // list.Add("b");
+// // list.Add("c");
+// // list.Print(2);
 //
-// var list = new List<string>();
+// // Select, Where, Min, Max
+// // First, Last, 
 //
-// list.Add("a");
-// list.Add("b");
-// list.Add("c");
-// list.Print(2);
-
-// Select, Where, Min, Max
-// First, Last, 
-
-// var faker = new Faker<User>();
+// // var faker = new Faker<User>();
+// //
+// // int i = 1;
+// //
+// // faker.RuleFor(x => x.Id, f => i++)
+// // 	.RuleFor(x => x.Name, f => f.Name.FirstName())
+// // 	.RuleFor(x => x.Bans, f => Random.Shared.Next(0, 5))
+// // 	.RuleFor(x => x.Login, f => f.Internet.Email())
+// // 	.RuleFor(x => x.RegDate, x => x.Date.Past())
+// // 	.RuleFor(x => x.Rating, x => Random.Shared.NextSingle());
+// //
+// // List<User> users = faker.Generate(5);
+// //
+// // foreach (var user in users)
+// // {
+// // 	PrintUser(user);
+// // 	Console.WriteLine("---------------------");
+// // }
+// //
+// // Console.WriteLine("---------------------");
+// // Console.WriteLine("---------------------");
 //
-// int i = 1;
+// // First & FirstOrDefault, Last & LastOrDefault
+// // {
+// // 	string name = Console.ReadLine()!;
+// //
+// // 	User? foundUser = users.FirstOrDefault(x => x.Name == name);
+// //
+// // 	if (foundUser == null)
+// // 	{
+// // 		Console.WriteLine("User not found");
+// // 	}
+// // 	else
+// // 	{
+// // 		Console.WriteLine("Found user: ");
+// //
+// // 		PrintUser(foundUser);
+// // 	}
+// // }
 //
-// faker.RuleFor(x => x.Id, f => i++)
-// 	.RuleFor(x => x.Name, f => f.Name.FirstName())
-// 	.RuleFor(x => x.Bans, f => Random.Shared.Next(0, 5))
-// 	.RuleFor(x => x.Login, f => f.Internet.Email())
-// 	.RuleFor(x => x.RegDate, x => x.Date.Past())
-// 	.RuleFor(x => x.Rating, x => Random.Shared.NextSingle());
+// // Where
+// // {
+// // 	float rating = float.Parse(Console.ReadLine()!);
+// //
+// // 	// deferred execution
+// // 	var filtered = users.Where(x => x.Rating > rating);
+// //
+// // 	foreach (var user in filtered)
+// // 	{
+// // 		
+// // 	}
+// // }
 //
-// List<User> users = faker.Generate(5);
+// //  MyWhere
+// // {
+// // 	var list = new List<int> { 10, -13, 20, -30, -40, 42, 50, 60, 61, -69, 70, -80, 99 };
+// // 	
+// // 	var negativeNumbers = list.MyWhere(x =>
+// // 	{
+// // 		Console.WriteLine("iteration");
+// // 	
+// // 		return x < 0;
+// // 	});
+// //
+// // 	// foreach (var number in negativeNumbers)
+// // 	// {
+// // 	// 	Console.WriteLine(number);
+// // 	// }
+// // }
 //
-// foreach (var user in users)
 // {
-// 	PrintUser(user);
-// 	Console.WriteLine("---------------------");
+// 	// IEnumerable<int> vals = EnumerableExtensions.GetValues();
+// 	//
+// 	// var enumerator = vals.GetEnumerator();
+// 	//
+// 	// while (enumerator.MoveNext())
+// 	// {
+// 	// 	var item = enumerator.Current;
+// 	//
+// 	// 	Console.WriteLine(item);
+// 	// }
 // }
 //
-// Console.WriteLine("---------------------");
-// Console.WriteLine("---------------------");
-
-// First & FirstOrDefault, Last & LastOrDefault
 // {
-// 	string name = Console.ReadLine()!;
-//
-// 	User? foundUser = users.FirstOrDefault(x => x.Name == name);
-//
-// 	if (foundUser == null)
-// 	{
-// 		Console.WriteLine("User not found");
-// 	}
-// 	else
-// 	{
-// 		Console.WriteLine("Found user: ");
-//
-// 		PrintUser(foundUser);
-// 	}
-// }
-
-// Where
-// {
-// 	float rating = float.Parse(Console.ReadLine()!);
-//
-// 	// deferred execution
-// 	var filtered = users.Where(x => x.Rating > rating);
-//
-// 	foreach (var user in filtered)
-// 	{
-// 		
-// 	}
-// }
-
-//  MyWhere
-// {
-// 	var list = new List<int> { 10, -13, 20, -30, -40, 42, 50, 60, 61, -69, 70, -80, 99 };
+// 	var list = new List<int> { 10, 13, 20, -30, -40};
 // 	
 // 	var negativeNumbers = list.MyWhere(x =>
 // 	{
@@ -82,55 +111,48 @@ using Bogus;
 // 		return x < 0;
 // 	});
 //
-// 	// foreach (var number in negativeNumbers)
-// 	// {
-// 	// 	Console.WriteLine(number);
-// 	// }
+// 	// list.Skip(5).Take(2);
+// 	// list.SkipWhile(x => x > 0);
+// 	// list.TakeWhile(x => x > 0);
+//
+// 	bool result = list.Any(x => x == -30);
+// 	
+// 	IEnumerable<int> with69 = list.Append(-69);
+//
+// 	foreach (var item in with69)
+// 	{
+// 		Console.WriteLine(item);
+// 	}
+// }
+//
+// void PrintUser(User user)
+// {
+// 	Console.WriteLine(user.Id);
+// 	Console.WriteLine(user.Login);
+// 	Console.WriteLine(user.Name);
+// 	Console.WriteLine(user.RegDate);
+// 	Console.WriteLine(user.Bans);
+// 	Console.WriteLine(user.Rating);
 // }
 
+using System.Dynamic;
+using System.Text.Json;
+
+var jsonObject = new
 {
-	// IEnumerable<int> vals = EnumerableExtensions.GetValues();
-	//
-	// var enumerator = vals.GetEnumerator();
-	//
-	// while (enumerator.MoveNext())
-	// {
-	// 	var item = enumerator.Current;
-	//
-	// 	Console.WriteLine(item);
-	// }
+	Test1 = 42,
+	Test2 = "String"
+};
+
+var json = JsonSerializer.Serialize(jsonObject);
+
+dynamic? obj = JsonSerializer.Deserialize<ExpandoObject>(json);
+
+if (obj is null)
+{
+	return;
 }
 
-{
-	var list = new List<int> { 10, 13, 20, -30, -40};
-	
-	var negativeNumbers = list.MyWhere(x =>
-	{
-		Console.WriteLine("iteration");
-	
-		return x < 0;
-	});
+Console.WriteLine(obj.Test1);
+Console.WriteLine(obj.Test2);
 
-	// list.Skip(5).Take(2);
-	// list.SkipWhile(x => x > 0);
-	// list.TakeWhile(x => x > 0);
-
-	bool result = list.Any(x => x == -30);
-	
-	IEnumerable<int> with69 = list.Append(-69);
-
-	foreach (var item in with69)
-	{
-		Console.WriteLine(item);
-	}
-}
-
-void PrintUser(User user)
-{
-	Console.WriteLine(user.Id);
-	Console.WriteLine(user.Login);
-	Console.WriteLine(user.Name);
-	Console.WriteLine(user.RegDate);
-	Console.WriteLine(user.Bans);
-	Console.WriteLine(user.Rating);
-}
---
+++ Linq2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Text.Json;
+
+var users = new List<User>
+{
+	new User
+	{
+		Id = 3,
+		Login = "snusoed",
+		Name = "Analbek"
+	},
+	new User
+	{
+		Id = 1,
+		Login = "abibas",
+		Name = "Abbas Ali"
+	},
+	new User
+	{
+		Id = 2,
+		Login = "adolf",
+		Name = "Maglot Yasper"
+	}
+}.ToArray();
+
+var sorted = users.OrderBy(u => u.Id).Select(x => x.Id);
+
+foreach (var id in sorted)
+{
+	Console.WriteLine(id);
+}
+
+var min = users.MinBy(x => x.Id);
+
+Console.WriteLine(min is null);
+
+// var names = users.Select(x => new
+// {
+// 	Id = x.Id,
+// 	Login = x.Login,
+// });
+//
+// foreach (var name in names)
+// {
+// 	Console.WriteLine(name);
+// }
+
+// var anon = new
+// {
+// 	Value = 42,
+// 	Data = "Hello World"
+// };
+
+// Foo(anon);
+// Foo2(anon);
+Console.Read();
+void Foo2(dynamic obj)
+{
+	Console.WriteLine(obj.Value);
+	Console.WriteLine(obj.Data);
+}
+
+void Foo(object obj)
+{
+	var type = obj.GetType();
+	var valueProperty = type.GetProperty("Value");
+	var dataProperty = type.GetProperty("Data");
+	
+	int value = (int)valueProperty!.GetValue(obj)!;
+	string data = (string)dataProperty!.GetValue(obj)!;
+	
+	Console.WriteLine(value);
+	Console.WriteLine(data);
+	
+	// Console.WriteLine(obj.Value);
+	// Console.WriteLine(obj.Data);
+}
+
+
+// List<int> list = [-1, -2, -3, -4, 1, 2, 3, 4];
+//
+//
+// var result = list.Where(x =>
+// {
+// 	Console.WriteLine("pos");
+//
+// 	return x > 0;
+// }).Where(x =>
+// {
+// 	Console.WriteLine("even");
+//
+// 	return x % 2 == 0;
+// }).ToArray();
+
+
+
+
+// var enumerator = result.GetEnumerator();
+//
+// while (enumerator.MoveNext())
+// {
+// 	Console.WriteLine(enumerator.Current);
+// }
+
+// foreach (var item in result)
+// {
+// 	Console.WriteLine($"item: {item}");
+// }
+
+// ToJson
+static class ObjectExtensions
+{
+	public static string ToJson(this object source)
+	{
+		return JsonSerializer.Serialize(source);
+	}
+}
+
+class User
+{
+	public int Id { get; set; }
+	public string? Login { get; set; }
+	public string? Name { get; set; }
+}
